cababdc506f3f516474708d9090e6289c83e3417
Stefan-6 <stefancolesnicenco6@gmail.com>
2026-05-21 12:43:38 +0200

    Create server.js

---
+++ gestion_incidencia/backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+// Permisos Angular
+app.use(cors());
+
+// Ruta que entrega el JSON de prueba
+app.get('/api/prueba', (req, res) => {
+    res.json({ 
+        mensaje: "¡Hola desde el Backend de Node! El cerebro está funcionando." 
+    });
+});
+
+// Arrancamos el servidor de forma global para Docker
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor Backend corriendo en el puerto ${PORT}`);
+});
